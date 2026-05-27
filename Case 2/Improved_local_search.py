0c879e9e83d5a730151e165c3854c6d3f3dc0747
Shuagib Ibrahim <shuagibibrahim@hotmail.com>
2026-05-27 10:31:16 +0200

    Improved version with increment

--- Case 2/Improved_local_search.py
+++ Case 2/Improved_local_search.py
@@ -1,4 +1,3 @@
-
 #from tabulate import tabulate
 import math
 from tabulate import tabulate
@@ -8,13 +7,14 @@ import time
 
 class Problem:
     def __init__(self,w:list[int],lb: list[list[int]], dis: list[tuple], team:int, stud:int, max_amount, min_amount):
+        """specifies the data structure to represent the particular instance of the problem to solve"""
         self.weight = w #Weights
         self.label = lb #Label values lb[student][attribute]
         self.dis = dis #Disagreement between s_x and s_y (s_x,s_y)
         self.team = team #The amount of teams
         self.stud = stud #The amount of students
-        self.max_amount = max_amount
-        self.min_amount = min_amount
+        self.max_amount = max_amount #Amount of Max size teams
+        self.min_amount = min_amount # Amounf of Min size of teams
 
 
     
@@ -31,13 +31,13 @@ class Problem:
         return cls(weights, labels, dis, team, stud, max_amount, min_amount)
 
 
-    def empty_solution(self):
+    def empty_solution(self): #Empty solution
         return Solution(self)
     
-    def construction_neighbourhood(self):
+    def construction_neighbourhood(self): #All allowed moved in construction
         return AddNeighbourhood(self)
     
-    def local_neighbourhood(self):
+    def local_neighbourhood(self):#All allowed moved in swaps
         return LocalNeighbourhood(self)
 
 
@@ -45,11 +45,17 @@ class Problem:
 
 
 class Solution:
+    """ Datastructure that represent the partial solution,  is an expression of the decision space. I"""
     def __init__(self,problem):
         self.problem = problem
-        self.team_list = [None] * problem.stud 
-        self.lookup = {t: [] for t in range(problem.team)} 
-        self.lb = 0 
+        self.team_list = [None] * problem.stud #List of teams index by a stident
+        self.lookup = [[] for _ in range(problem.team)] #
+        self.lb = 0
+        num_attr = len(problem.label[0])
+        self.team_labels = {
+            t: [dict() for _ in range(num_attr)]
+            for t in range(problem.team)
+        }
 
 
     def __str__(self):
@@ -58,31 +64,26 @@ class Solution:
     def copy_solution(self):
         new = Solution(self.problem)
         new.team_list = self.team_list.copy()
-        new.lookup = {t: students.copy() for t, students in self.lookup.items()}
-        
+        new.lookup = [students.copy() for students in self.lookup]
+        new.lb = self.lb
+        new.team_labels = {
+            t: [d.copy() for d in attrs]
+            for t, attrs in self.team_labels.items()
+        }
         return new
-
     
     def objective_value(self):
-        """" Calculating the objective function """
-        #Caluclate the objective value for all students
-        total = 0
-        for i in self.lookup: #Loop over the teams 
-            for id, w in enumerate(self.problem.weight): #Loop over weight both index and element
-                dis = set(self.problem.label[s][id] for s in self.lookup[i]) #Get  distinct values by computing it form labeel 
-                total += w* len(dis) #Do Mutilication with each weight and dinstct in label list 
-        return total
-    
+        return self.lb
+        
     def lower_bound(self):
         return -self.ub
     
     def save_solution(self, f):
-        """ Saving our file for checker.py """
         with open(f, "w") as file:
-            file.write(" ".join(map(str, self.team_list))) #Saving our solution files 
+            file.write(" ".join(map(str, self.team_list)))
 
     def cost_team(self,t):
-        """ Calcaulting the objective value for 1 team - Helper function in  LocalMove"""
+        #Calculate the objective value for all students
         total = 0
         for id, w in enumerate(self.problem.weight): #Loop over weight both index and elem
             dis = set(self.problem.label[s][id] for s in self.lookup[t]) #Get  distinct values for each team
@@ -93,10 +94,9 @@ class Solution:
   
 
 class AddMove:
-    """ All student in each team"""
     def __init__(self,stud,team,problem):
-        self.stud = stud  #student
-        self.team = team #Team
+        self.stud = stud 
+        self.team = team 
         self.lb_incr = None #lower bound incremenentation
         self.problem = problem
 
@@ -106,37 +106,47 @@ class AddMove:
 
    
     def lower_bound_increment(self, solution):
-            """ Lower bound measure affinity in each team. 
-                If a affinity increases, then we add our lower bound"""
-            if self.lb_incr is None:
-                    self.lb_incr = 0
-            for id, val in enumerate(self.problem.weight):
-                dis = set(self.problem.label[s][id] for s in solution.lookup[self.team]) 
-                if self.problem.label[self.stud][id] not in dis:
-                    self.lb_incr += val 
-            return self.lb_incr
-    
-    
+        counts = solution.team_labels[self.team]      
+        student_labels = self.problem.label[self.stud]
+        incr = 0
+        for a, label in enumerate(student_labels):
+            if counts[a].get(label, 0) == 0:          
+                incr += self.problem.weight[a]        
+        return incr
+        
+        
 
 
     def apply_move(self, solution):
-        solution.team_list[self.stud] = self.team #Adding a team to a list 
-        solution.lookup[self.team].append(self.stud) #The key is the team and append students index to pair
-        return solution 
+        # 1. Update the objective tracker 
+        solution.lb += self.lower_bound_increment(solution)
+        
+        # 2. Update label counts
+        student_labels = self.problem.label[self.stud]
+        for a, label in enumerate(student_labels):
+            counts = solution.team_labels[self.team][a]
+            counts[label] = counts.get(label, 0) + 1   # increment count
+        
+        # 3. Update lookup and team_list (your existing logic)
+        solution.team_list[self.stud] = self.team
+        solution.lookup[self.team].append(self.stud)
+        return solution
+
 
 class AddNeighbourhood:
     def __init__(self, Problem):
         self.problem = Problem
 
     def moves(self,solution):
-        min_size = min(len(solution.lookup[k]) for k in solution.lookup)
-        max_size = max(len(solution.lookup[k]) for k in solution.lookup)
-        for s,team in enumerate(solution.team_list):
-            if team is None: 
-                for t in solution.lookup: 
-                    if len(solution.lookup[t]) <= max_size and min_size == len(solution.lookup[t]): 
-                        for a,b in self.problem.dis:
-                         
+        """A move is only valid if it forfill our constrains"""
+        min_size = min(len(students) for students in solution.lookup)
+        max_size = max(len(students) for students in solution.lookup)
+        for s,team in enumerate(solution.team_list): #Iterate over teams 
+            if team is None: #If there is None
+                for t in range(len(solution.lookup)): #Loop over every student in the team 
+                    if len(solution.lookup[t]) <= max_size and min_size == len(solution.lookup[t]): #Making sure the smallest team get filled first
+                        for a,b in self.problem.dis: #Looking at par in disagreement
+                            #print(self.problem.dis)
                             if  (s == a and b in solution.lookup[t]) or (s == b and a in solution.lookup[t]): 
                                 break 
                         else:
@@ -166,42 +176,57 @@ class LocalMove:
     
 
     def objective_value_increment(self, solution):
-        if self.ob_incr is 0:
-            t1 = solution.team_list[self.s1]
-            t2 = solution.team_list[self.s2]
-            
-            obj_before = solution.cost_team(t1) + solution.cost_team(t2)
-            
-            self.apply_move(solution)
-            
-            after = solution.cost_team(t1) + solution.cost_team(t2)
-            
-            self.apply_move(solution) 
-
-            self.ob_incr  = after - obj_before
-                    
-            return  self.ob_incr 
-
-
-
-    def apply_move(self, solution):
-  
         t1 = solution.team_list[self.s1]
         t2 = solution.team_list[self.s2]
-        
+        s1_labels = solution.problem.label[self.s1]   
+        s2_labels = solution.problem.label[self.s2]
+        incr = 0
+        for a in range(len(s1_labels)):
+            l1 = s1_labels[a]  
+            l2 = s2_labels[a]   
+            w = solution.problem.weight[a]
+            if l1 == l2:
+                continue        
+            c1 = solution.team_labels[t1][a]   
+            c2 = solution.team_labels[t2][a]   
+            # t1: s1 leaves, s2 arrives
+            if c1.get(l1, 0) == 1:  
+                incr -= w           
+            if c1.get(l2, 0) == 0:   # l2 new to t1
+                incr += w
+            # t2: s2 leaves, s1 arrives
+            if c2.get(l2, 0) == 1:   # s2 is last holder of l2 in t2
+                incr -= w
+            if c2.get(l1, 0) == 0:   # l1 new to t2
+                incr += w
+        return incr
 
-        solution.team_list[self.s2] = t1
+    def apply_move(self, solution):                              
+        solution.lb += self.objective_value_increment(solution)
+        
+        t1 = solution.team_list[self.s1]
+        t2 = solution.team_list[self.s2]
+        s1_labels = solution.problem.label[self.s1]             
+        s2_labels = solution.problem.label[self.s2]             
+        
+        for a in range(len(s1_labels)):
+            l1, l2 = s1_labels[a], s2_labels[a]
+            c1 = solution.team_labels[t1][a]
+            c2 = solution.team_labels[t2][a]
+            c1[l1] -= 1
+            if c1[l1] == 0: del c1[l1]
+            c2[l2] -= 1
+            if c2[l2] == 0: del c2[l2]
+            c1[l2] = c1.get(l2, 0) + 1
+            c2[l1] = c2.get(l1, 0) + 1
+        
         solution.team_list[self.s1] = t2
-        
-
+        solution.team_list[self.s2] = t1
         solution.lookup[t1].remove(self.s1)
         solution.lookup[t2].append(self.s1)
-        
-  
         solution.lookup[t2].remove(self.s2)
         solution.lookup[t1].append(self.s2)
         return solution
-    
 
 
 class LocalNeighbourhood:
@@ -217,7 +242,7 @@ class LocalNeighbourhood:
 
     def moves(self, solution):
         """ All the applied swappeds avaible for us """
-        teams = list(solution.lookup.keys())
+        teams = list(range(len(solution.lookup)))
         for i in range(len(teams)):
             for j in range(i + 1, len(teams)):
                 t1 = teams[i]
@@ -287,29 +312,4 @@ def linear(temp, k):
 
 def logarithmic(temp, k, temp0=1000):
     return temp0 / math.log(k + 2)
-
-
-
-def simulated_annealing(problem, schedule, temp=5, inner=100, temp_min=0.01, time_limit=60):
-    s = greedy_algorithm(problem)
-    neigh = problem.local_neighbourhood()
-    best = s.copy_solution()
-    best_val = s.objective_value()
-    
-    start = time.time()
-    k = 0
-    while temp > temp_min and time.time() - start < time_limit:  
-        for _ in range(inner):
-            move = random.choice(list(neigh.moves(s)))
-            delta = move.objective_value_increment(s)
-            if delta < 0 or random.random() < math.exp(-delta / temp):
-                move.apply_move(s)
-                val = s.objective_value()
-                if val < best_val:
-                    best, best_val = s.copy_solution(), val
-        k += 1
-        temp = schedule(temp, k)
-        
-    return best
-
 
